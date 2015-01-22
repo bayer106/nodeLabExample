66d4d7f6cad974a94a1a44e6f1683df75b6ddb5c
bayer106 <bayer106@morris.umn.edu>
2015-01-22 13:25:15 -0600

    Working on task 1

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/kittens', function(req, res){
-    res.send("Page under construction!");
+    res.send("IT'S SO FLUFFY!");
 });
 
 app.get('/about', function(req, res){
@@ -57,7 +57,7 @@ app.get('/', function(req, res){
 
 // Wildcard catches requests for non-existent routes or files and responds with a 404 message (or an html page if you want to make a custom one!)
 app.get('*', function(req, res){
-    res.send("404");
+    res.send("It ain't here, Frodo!");
 //    res.sendFile('404.html', options);
 });
 
